659cadd792b133dd44467a5cfa19f5d8ac8955fe
Anja Dundov <anjadundov@anjas-air.wireless.rit.edu>
2025-03-24 12:32:11 +0100

    push

---
+++ src/Graph.java
@@ -0,0 +1,9 @@
+public interface Graph<E> {
+    void add(E value);
+    boolean contains(E value);
+    int size();
+    void connectDirected(E a, E b);
+    void connectUndirected(E a, E b);
+    boolean connect(E a, E b);
+    
+}
